b1adfb3f275e954c7ee4fe1da797fe153bf15c76
Enrico Zanotti <enrico.zan07@gmail.com>
2024-01-09 11:32:44 +0100

    Translated Ex 3

--- Esercizi 2-4/Esercizio 3.c
+++ Esercizi 2-4/Esercizio 3.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 
 int main(void) {
-    int qtaNumeri;
+    int numAmount;
 
-    //Richiedere l'input all'utente
+    //Request user input
     printf("Determinare la quantita' di numeri da inserire : ");
-    scanf("%d", &qtaNumeri);
+    scanf("%d", &numAmount);
 
-    /*Variabili utilizzate */
-    int sommaPositivi = 0;
-    int sommaNegativi = 0;
-    int qtaPositivi = 0;
-    int qtaNegativi = 0;
-    double mediaPositivi, mediaNegativi;
-    int numeroCorrente;
+    //Variables
+    int sumPositive = 0;
+    int sumNegative = 0;
+    int quantityPositive = 0;
+    int quantityNegative = 0;
+    double positiveAverage, negativeAverage;
+    int currentNumber;
 
-    for(unsigned int iterator = 0; iterator < qtaNumeri; iterator++)
+    // Iterate trough all the requested number inputs
+    for(unsigned int iterator = 0; iterator < numAmount; iterator++)
     {
         printf("Inserire il numero %d : ", iterator + 1);
-        scanf("%d", &numeroCorrente);
+        scanf("%d", &currentNumber);
 
-        /*In base al segno del numero incrementare la quantita' di numeri
-            e la somma dei valori assoluti dei numeri relativi*/
-        if(numeroCorrente >= 0) {
-            qtaPositivi++;
-            sommaPositivi += numeroCorrente;
-        } else if(numeroCorrente < 0) {
-            qtaNegativi++;
-            sommaNegativi += (numeroCorrente * -1);
+        /* Depending on the sign and increment quantity of numbers, sum
+            of absolute values and the relative numbers.*/
+        if(currentNumber >= 0) {
+            quantityPositive++;
+            sumPositive += currentNumber;
+        } else if(currentNumber < 0) {
+            quantityNegative++;
+            sumNegative += (currentNumber * -1);
         }
     }
 
-    /*Calcolo della media dei numeri positivi e negativi*/
-    mediaPositivi = (double)sommaPositivi / (double)qtaPositivi;
-    mediaNegativi = (double)sommaNegativi / (double)qtaNegativi;
+    //Calculate the averages
+    positiveAverage = (double)sumPositive / (double)quantityPositive;
+    negativeAverage = (double)sumNegative / (double)quantityNegative;
 
-    //Stampa risultati
+    //Output results
     printf(
         "Quantita' numeri positivi : %d\n\
         Somma numeri positivi : %d\n\
@@ -43,6 +44,6 @@ int main(void) {
         Quantita' numeri negativi : %d\n\
         Somma numeri negativi : %d\n\
         Media numeri negativi : %lf\n\n", 
-        qtaPositivi, sommaPositivi, mediaPositivi, qtaNegativi, sommaNegativi, mediaNegativi
+        quantityPositive, sumPositive, positiveAverage, quantityNegative, sumNegative, negativeAverage
     );
 }
