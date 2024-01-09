329010fb13c37d5fc30b19ed4767f3a5741a6f80
Enrico Zanotti <enrico.zan07@gmail.com>
2024-01-09 11:42:42 +0100

    Translated Ex 4

--- Esercizi 2-4/Esercizio 4.c
+++ Esercizi 2-4/Esercizio 4.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 
-/*
- *Funzione che ritorna se un numero e' primo o no
+/*Returns wether the parameter is prime or not
 */
-int numeroPrimo(int numero) {
-    //Inizializza una variabile considerando il numero come primo
-    int primo = 1;
+int numeroPrimo(int number) {
+    //Consider the number as prime initially
+    int prime = 1;
 
-    /*Ciclo che controlla tutti i numeri da 2 a numero - 1 se sono divisibili per il numero inserito
-      se si, il numero non e' primo*/
-    for(int iteratore = 2; iteratore < numero && primo; iteratore++){
-        //Se si trova un divisore il numero non e' primo
-        if(numero % iteratore == 0)
-            primo = 0;
+    /*Check all numbers form 2 to number - 1 to check if they are divisible by the parameter.
+        if a divider is found the number isn't prime.*/
+    for(int iterator = 2; iterator < number && prime; iterator++){
+        //In case a divider is found the number isn't prime so exit the loop
+        if(number % iterator == 0)
+            prime = 0;
     }
 
-    //Ritornare lo stato della variabile
-    return primo;
+    //Return the boolean
+    return prime;
 }
 
 int main(void) {
-    //Contiene il nuero inserito e se sommare il numero o no
-    int numeroInserito;
-    int sommareNumero = 1; //Questa variabile si utilizza per stampare i numeri alternati
+    //Contains the number inserted and wether to sum it or not
+    int userInput;
+    int sumNumber = 1; //Prints the altermnating numbers
 
-    //Valore di uscita
-    int sommaNumeriPrimi = 0;
+    //Output value
+    int primeSum = 0;
 
-    /*Controllo sul numero inserito dall'utente*/
+    //Run a check on the number the user inputs
     do {
-        //Richiesta di input
+        //Input
         printf("Inserire il numero di arrivo : ");
-        scanf("%d", &numeroInserito);
+        scanf("%d", &userInput);
 
-        //Stampa di un messaggio di errore nel caso il numero inserito non sia positivo
-        if(numeroInserito <= 0)
+        if(userInput <= 0) //Error message
             printf("Errore : il numero inserito deve essere positivo");
-    } while(numeroInserito <= 0);
+    } while(userInput <= 0);
 
-    /*Ricerca dei primi N numeri primi e somma di essi in modo alternato*/
-    for(int iteratore = 0; iteratore <= numeroInserito; iteratore++){
-        if(numeroPrimo(iteratore)){
-            if(sommareNumero) 
-                sommaNumeriPrimi += iteratore;
-            sommareNumero = !sommareNumero;
+    /*Find the first N numbers and sum the alternating ones*/
+    for(int iterator = 0; iterator <= userInput; iterator++){
+        if(numeroPrimo(iterator)){
+            if(sumNumber) 
+                primeSum += iterator;
+            sumNumber = !sumNumber;
         }
     }
 
-    //Stampa del risultato
-    printf("La somma dei primi %d numeri primi alternati e' %d\n", numeroInserito, sommaNumeriPrimi);
+    //Print the output
+    printf("La somma dei primi %d numeri primi alternati e' %d\n", userInput, primeSum);
 }
